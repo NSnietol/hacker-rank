a66bb80e28e4ce7b315a0b9a175b4a583cb41b56
NSnietol <nilson-sn@outlook.com>
2018-02-25 20:40:08 -0500

    The world of Numbers

---
+++ Python/The World of Numbers.sh
@@ -0,0 +1,10 @@
+##
+##Given two integers, X and Y, find their sum, difference, product, and quotient.
+##Two lines containing one integer each ( Xand Y, respectively).
+
+read num1
+read num2
+echo "$(($num1+$num2))"
+echo "$(($num1-$num2))"
+echo "$(($num1*$num2))"
+echo "$(($num1/$num2))"
