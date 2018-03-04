b281ab5e3f051553ee5b4c2ea81f98b5c7b7389d
NSnietol <nilson-sn@outlook.com>
2018-03-04 09:15:25 -0500

    Update Find the runner-up

--- Python/Find the Runner-Up Score.py
+++ Python/Find the Runner-Up Score.py
@@ -1,7 +1,17 @@
 """
+
+Python 3.x
 Given the participants' score sheet for your University Sports Day, 
 you are required to find the runner-up score. 
 You are given n scores. Store them in a list and find the score of the runner-up.
+
+
+Input Format
+
+The first line contains n. The second line contains an array []  of n  integers each separated by a space.
+
+URL=https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
 """
 
 if __name__ == '__main__':
