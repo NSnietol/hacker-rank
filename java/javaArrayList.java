d56dc473cbb60c897d97c622509e5d8594ed5e50
NSnietol <nilson-sn@outlook.com>
2018-12-16 19:48:54 -0500

    add hot fix

--- java/javaArrayList.java
+++ java/javaArrayList.java
@@ -11,3 +11,49 @@ The first line has an integer n. In each of the next n  lines there will be an i
 Output Format
 In each line, output the number located in y position of  x line. If there is no such position, just print "ERROR!"
 * */
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner scanner = new Scanner(System.in);
+            
+        int nArrays = Integer.parseInt(scanner.nextLine());
+        
+        List<List<String>> list = new ArrayList<>();
+    
+        for (int i = 0; i < nArrays; i++) {
+        
+         list.add(Arrays.asList(scanner.nextLine().split(" ")));
+        }
+
+            int nPoints = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < nPoints; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            try{
+                   System.out.println(list.get( x-1).get( y ));
+            }catch(IndexOutOfBoundsException e){
+
+                System.out.println("ERROR!");
+            
+            }    
+
+
+            
+
+        }
+    }
+}
+
